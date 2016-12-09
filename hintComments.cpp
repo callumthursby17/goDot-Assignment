3b50fdaac59ef8d460fef200cbafb799711cc05f
95jr12 <302335@student.lincolncollege.ac.uk>
2016-12-09 14:55:43 +0000

    Update hintComments.cpp

--- hintComments.cpp
+++ hintComments.cpp
@@ -1,8 +1,11 @@
 #include "hintComments.h"
+#include "scene/gui/text_edit.h"
 
 class hintComments {
 	void hintComments::set_Hint_Comment() {
-
+		String *Insertedtext = "Testcase";
+		TextEdit editor = new TextEdit();
+		editor._insert_text_at_cursor(Insertedtext);
 	}
 	void hintComments::check_Hash_Marker() {
 
