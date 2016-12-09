027da3e8c5825c3cb5fed7cd961e0c155de88002
95jr12 <302335@student.lincolncollege.ac.uk>
2016-12-09 13:32:02 +0000

    Add files via upload

---
+++ hintComments.cpp
@@ -0,0 +1,13 @@
+#include "hintComments.h"
+
+class hintComments {
+	void hintComments::set_Hint_Comment() {
+
+	}
+	void hintComments::check_Hash_Marker() {
+
+	}
+	string hintComments::find_Hint() {
+
+	}
+};
---
+++ hintComments.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <string>
+#include "error_list.h"
+#include "typedefs.h"
+#include "text_edit.h"
+
+class hintComments {
+	void hintComments::set_Hint_Comment();
+	void hintComments::check_Hash_Marker();
+	string hintComments::find_Hint();
+};
