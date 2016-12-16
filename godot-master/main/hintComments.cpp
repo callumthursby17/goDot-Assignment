b23d83ed6cbd971974f825b9554a3252c2e405e7
95jr12 <302335@student.lincolncollege.ac.uk>
2016-12-16 14:46:43 +0000

    Add files via upload

---
+++ godot-master/main/hintComments.cpp
@@ -0,0 +1,16 @@
+#include "hintComments.h"
+#include "scene/gui/text_edit.h"
+
+class hintComments {
+	void hintComments::set_Hint_Comment() {
+		String *Insertedtext = "Testcase";
+		TextEdit editor = new TextEdit();
+		editor._insert_text_at_cursor(Insertedtext);
+	}
+	void hintComments::check_Hash_Marker() {
+
+	}
+	string hintComments::find_Hint() {
+
+	}
+};
---
+++ godot-master/main/hintComments.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <string>
+#include "core/error_list.h"
+#include "core/typedefs.h"
+#include "scene/gui/text_edit.h"
+
+class hintComments {
+	void hintComments::set_Hint_Comment();
+	void hintComments::check_Hash_Marker();
+	string hintComments::find_Hint();
+};
