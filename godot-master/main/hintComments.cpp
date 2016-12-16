e813eb19c9498b9f2b304a14f1c9480f54110753
95jr12 <302335@student.lincolncollege.ac.uk>
2016-12-16 14:50:40 +0000

    Add files via upload

--- godot-master/main/hintComments.cpp
+++ godot-master/main/hintComments.cpp
@@ -2,10 +2,10 @@
 #include "scene/gui/text_edit.h"
 
 class hintComments {
-	void hintComments::set_Hint_Comment() {
-		String *Insertedtext = "Testcase";
-		TextEdit editor = new TextEdit();
-		editor._insert_text_at_cursor(Insertedtext);
+	void hintComments::set_Hint_Comment(String LineofText) {
+		//Search through possible statements to test if lineoftext matches any of them.
+		//if it does then get a description of the statement.
+		//output the statement description at cursor position.
 	}
 	void hintComments::check_Hash_Marker() {
 
--- godot-master/main/hintComments.h
+++ godot-master/main/hintComments.h
@@ -5,7 +5,7 @@
 #include "scene/gui/text_edit.h"
 
 class hintComments {
-	void hintComments::set_Hint_Comment();
+	void hintComments::set_Hint_Comment(String LinofText);
 	void hintComments::check_Hash_Marker();
 	string hintComments::find_Hint();
 };
